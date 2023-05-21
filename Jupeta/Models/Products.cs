be432887c083cd8d5a11b6838da2eb608674cb65
Steve Ofori <oforistep@gmail.com>
2023-05-21 00:48:36 +0000

    logging added

---
+++ Jupeta/Models/Products.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace Jupeta.Models
+{
+    public class Products
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+        [BsonElement("productName")]
+        public string ProductName { get; set; } = string.Empty;
+        [BsonElement("description")]
+        public string Description { get; set; } = string.Empty;
+        [BsonElement("summary")]
+        public string Summary { get; set; } = string.Empty;
+        [BsonElement("price")]
+        public string Price { get; set; } = string.Empty;
+        [BsonElement("isAvailable")]
+        public bool IsAvailable { get; set; }
+        [BsonElement("addedAt")]
+        [BsonDateTimeOptions]
+        public DateTime AddedAt { get; set; }
+    }
+}
